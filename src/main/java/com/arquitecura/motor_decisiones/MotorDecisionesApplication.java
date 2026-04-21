246f43dffab9edbda9f39182e12fe4313f331173
Crhistian Ziegler Pacori Paucar <65138605+Cristian-hash@users.noreply.github.com>
2026-04-21 05:59:53 -0500

    ladrillo15-init: chasis base Spring Boot 3, Java 21 y conexion a PostgreSQL

---
+++ src/main/java/com/arquitecura/motor_decisiones/MotorDecisionesApplication.java
@@ -0,0 +1,13 @@
+package com.arquitecura.motor_decisiones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorDecisionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotorDecisionesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arquitecura/motor_decisiones/MotorDecisionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arquitecura.motor_decisiones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorDecisionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
