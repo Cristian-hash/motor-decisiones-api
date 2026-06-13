85a16c10a6109ca2f228649bb56ae5d5dd26001c
Crhistian Ziegler Pacori Paucar <65138605+Cristian-hash@users.noreply.github.com>
2026-06-13 05:51:45 -0500

    lad61(parte 2)-feat: revision de los archivos de Kafka conocer su implementacion

--- src/main/java/com/arquitectura/motor_decisiones/service/EvaluacionService.java
+++ src/main/java/com/arquitectura/motor_decisiones/service/EvaluacionService.java
@@ -20,11 +20,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDateTime;
 
-
-
-
-
-
 @Service
 public class EvaluacionService {
     //1 Agenda de especialistas
