a53f69a187a221e4d94bc65a1fd57b85a087ae00
Crhistian Ziegler Pacori Paucar <65138605+Cristian-hash@users.noreply.github.com>
2026-06-23 06:21:42 -0500

    lad65-docs: evidencia vulnerabilidad de race condition en EvaluacionService durante doble submit

--- src/main/java/com/arquitectura/motor_decisiones/service/gamificacion/PuntosNormalStrategy.java
+++ src/main/java/com/arquitectura/motor_decisiones/service/gamificacion/PuntosNormalStrategy.java
@@ -1,13 +1,15 @@
 package com.arquitectura.motor_decisiones.service.gamificacion;
 
+import com.arquitectura.motor_decisiones.entity.Leccion;
 import com.arquitectura.motor_decisiones.entity.Progreso;
+import com.arquitectura.motor_decisiones.entity.Usuario;
 import org.springframework.stereotype.Component;
 
 @Component("puntosNormalStrategy")
 public class PuntosNormalStrategy implements CalculadoraPuntosStrategy {
 
     @Override
-    public int calcularPuntos(Progreso progreso){
-     return progreso.getLeccion().getPuntosRecompensa();
+    public int calcularPuntos(Usuario usuario, Leccion leccion){
+     return leccion.getPuntosRecompensa();
     }
 }
--- src/main/java/com/arquitectura/motor_decisiones/service/gamificacion/PuntosRachaStrategy.java
+++ src/main/java/com/arquitectura/motor_decisiones/service/gamificacion/PuntosRachaStrategy.java
@@ -1,13 +1,15 @@
 package com.arquitectura.motor_decisiones.service.gamificacion;
 
+import com.arquitectura.motor_decisiones.entity.Leccion;
 import com.arquitectura.motor_decisiones.entity.Progreso;
+import com.arquitectura.motor_decisiones.entity.Usuario;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PuntosRachaStrategy implements CalculadoraPuntosStrategy {
 
     @Override
-    public int calcularPuntos(Progreso progreso){
+    public int calcularPuntos(Usuario usuario, Leccion leccion){
         return 20;
     }
 }
