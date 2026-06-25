c86b6b6fd3f611b693166b60c73cd8c3ff97eb2f
Crhistian Ziegler Pacori Paucar <65138605+Cristian-hash@users.noreply.github.com>
2026-06-25 05:49:32 -0500

    lad67-feat: explora PESSIMISTIC_WRITE con SELECT FOR UPDATE para entender bloqueos a nivel de motor SQL

--- src/main/java/com/arquitectura/motor_decisiones/repository/UsuarioRepository.java
+++ src/main/java/com/arquitectura/motor_decisiones/repository/UsuarioRepository.java
@@ -1,10 +1,19 @@
 package com.arquitectura.motor_decisiones.repository;
 
 import com.arquitectura.motor_decisiones.entity.Usuario;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findByEmail(String email);
+
+    // NUEVO MÉTODO ARQUITECTÓNICO: Bloqueo Pesimista
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT u FROM Usuario u WHERE u.id = :id")
+    Optional<Usuario> findByIdForUpdate(@Param("id") Long id);
 }
