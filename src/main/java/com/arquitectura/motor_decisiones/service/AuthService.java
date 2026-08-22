7ec22f7350f6406174bb7f986c9fb16e19ac5e32
Crhistian Ziegler Pacori Paucar <65138605+Cristian-hash@users.noreply.github.com>
2026-08-22 04:55:03 -0500

    Modifica AuthResponseDTO para verificar contraseña con BCrypt

--- src/main/java/com/arquitectura/motor_decisiones/service/AuthService.java
+++ src/main/java/com/arquitectura/motor_decisiones/service/AuthService.java
@@ -4,29 +4,43 @@ import com.arquitectura.motor_decisiones.dto.AuthRequestDTO;
 import com.arquitectura.motor_decisiones.dto.AuthResponseDTO;
 import com.arquitectura.motor_decisiones.entity.Usuario;
 import com.arquitectura.motor_decisiones.repository.UsuarioRepository;
+import org.springframework.security.authentication.AuthenticationEventPublisher;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
     private UsuarioRepository usuarioRepository;
     private JwtService jwtService;
-    public AuthService(UsuarioRepository usuarioRepository,JwtService jwtService){
+    private AuthenticationManager authenticationManager;
+    public AuthService(UsuarioRepository usuarioRepository, JwtService jwtService, AuthenticationManager authenticationManager){
         this.usuarioRepository=usuarioRepository;
         this.jwtService=jwtService;
+        this.authenticationManager = authenticationManager;
     }
 
     public AuthResponseDTO login(AuthRequestDTO request){
-        // 1. Buscar al usuario por su email
-        Usuario usuario = usuarioRepository.findByEmail(request.email())
-                .orElseThrow(()-> new RuntimeException("Credenciales inválidas"));
+        try{
+            // 1. Delegamos el trabajo sucio al Jefe de Seguridad
+            // Él irá a PostgreSQL y usará BCrypt para saber si "123456" coincide con el Hash guardado.
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(request.email(),request.password())
+            );
 
-        // 2. Comparar la contraseña (Por ahora en texto plano, luego integraremos BCrypt)
-        if(!usuario.getPassword().equals(request.password())){
-            throw new RuntimeException("Credenciales invalidas");
+
+
+            Usuario usuario = usuarioRepository.findByEmail(request.email())
+                    .orElseThrow(()-> new RuntimeException("Credenciales inválidas"));
+
+            // 3. Si todo es correcto, generamos el "pasaporte"
+            // (Hoy es un string simulado, pronto usaremos la llave maestra del application.properties)
+            String tokenReal = jwtService.generateToken(usuario);
+            return new AuthResponseDTO(tokenReal);
+        }catch( Exception e ){
+            e.printStackTrace();
+            System.err.println("ERROR REAL EN LOGIN:"+e.getMessage());
+            throw new RuntimeException("",e);
         }
-        // 3. Si todo es correcto, generamos el "pasaporte"
-        // (Hoy es un string simulado, pronto usaremos la llave maestra del application.properties)
-        String tokenReal = jwtService.generateToken(usuario);
-        return new AuthResponseDTO(tokenReal);
     }
 }
