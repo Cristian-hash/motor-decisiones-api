5b782dcc909b7fd8f1bd3ceea6d2233acfcaef93
Crhistian Ziegler Pacori Paucar <65138605+Cristian-hash@users.noreply.github.com>
2026-09-18 04:56:48 -0500

    lad118 (parte1) feat: implementacion de enrutamiento inteligente post-login y penalizacion de XP por fuerza bruta

--- src/main/java/com/arquitectura/motor_decisiones/repository/ProgresoRepository.java
+++ src/main/java/com/arquitectura/motor_decisiones/repository/ProgresoRepository.java
@@ -3,8 +3,16 @@ package com.arquitectura.motor_decisiones.repository;
 import com.arquitectura.motor_decisiones.entity.Progreso;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 
 @Repository
 public interface ProgresoRepository extends JpaRepository<Progreso, Long> {
     boolean existsByUsuarioIdAndLeccionIdAndCompletadoTrue(Long usuarioId, Long leccionId);
+    @Query("SELECT COALESCE(MAX(p.leccion.id), 0) " +
+            "FROM Progreso p " +
+            "WHERE p.usuario.id = :usuarioId " +
+            "AND p.completado = true")
+    Long findUltimaLeccionCompletada(@Param("usuarioId") Long usuarioId);
 }
